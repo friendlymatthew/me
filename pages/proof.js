eeae461809cfd7da9f797d408accab8d8be9b8f4
matthew kim <friendlymatthew@berkeley.edu>
2022-08-30 17:18:25 -0400

    added proof again

--- pages/proof.js
+++ pages/proof.js
@@ -1,6 +1,6 @@
 export default function Proof() {
 	return (
-		<div className="grid grid-cols-1 place-items-center h-screen">
+		<div className="grid grid-cols-1 place-items-center">
 			<img src="/proof.png" className="w-8/12"></img>
 		</div>
 	);
