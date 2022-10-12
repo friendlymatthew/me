85e146e69bbd97e3879b407769a00c131f39cf76
matthew kim <friendlymatthew@berkeley.edu>
2022-10-12 15:58:58 -0400

    added func to buttons

--- pages/index.js
+++ pages/index.js
@@ -147,7 +147,10 @@ export default function Home() {
 								>
 									Download Resume
 								</a>
-								<a className="cursor-pointer group-hover:ring-4 bg-[#ffdb13] group-hover:bg-[#0053d6] transition ease-in duration-200 h-12 w-12 flex justify-center items-center  rounded-full">
+								<a
+									onClick={handleResume}
+									className="cursor-pointer group-hover:ring-4 bg-[#ffdb13] group-hover:bg-[#0053d6] transition ease-in duration-200 h-12 w-12 flex justify-center items-center  rounded-full"
+								>
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
@@ -164,30 +167,30 @@ export default function Home() {
 									</svg>
 								</a>
 							</div>
-
-							<div className="col-span-2  flex space-x-4 items-center cursor-pointer group py-2">
-								<Link href="/projects">
+							<Link href="/projects">
+								<div className="col-span-2  flex space-x-4 items-center cursor-pointer group py-2">
 									<div className="group-hover:underline underline-offset-2 ">
 										See Projects
 									</div>
-								</Link>
-								<a className="cursor-pointer group-hover:ring-4 bg-[#ffdb13] group-hover:bg-[#0053d6] transition ease-in duration-200 h-12 w-12 flex justify-center items-center  rounded-full">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth="1.5"
-										stroke="currentColor"
-										className="w-6 h-6 group-hover:text-white duration-100 ease-in transition "
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-										/>
-									</svg>
-								</a>
-							</div>
+
+									<a className="cursor-pointer group-hover:ring-4 bg-[#ffdb13] group-hover:bg-[#0053d6] transition ease-in duration-200 h-12 w-12 flex justify-center items-center  rounded-full">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="w-6 h-6 group-hover:text-white duration-100 ease-in transition "
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+											/>
+										</svg>
+									</a>
+								</div>
+							</Link>
 						</div>
 
 						<div className="my-8">
